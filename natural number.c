1366252fde305d46781e3e7677003605ff5bff31
Praveenyaligar <praveenyaligar36@gmail.com>
2026-08-31 19:51:32 +0530

    Add program to check if a number is natural

---
+++ natural number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter a number : ");
+    scanf("%d",&x);
+    if(x>0)
+    printf("Natural number");
+    else
+    printf("Not a natural number");
+    
+}
